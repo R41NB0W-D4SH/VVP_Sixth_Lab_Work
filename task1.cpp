c667dba961ada3bddffcf2d10d6c39ee5056112e
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-09-30 13:41:18 +0300

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int n, n1;
+
+	cout << "Введите количество секунд прошедших с начала суток" << endl;
+	cin >> n;
+	cout << "Количество секунд прошедших с начала последней минуты равно " << n % 60 << endl;
+
+	system("pause");
+	return 0;
+}
