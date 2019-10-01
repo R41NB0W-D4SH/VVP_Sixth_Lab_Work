4ffc8d57ab4b0708f7eb24c06ee57f8c7bf0fbfe
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-10-01 15:08:09 +0300

    Update task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int n, m;
+
+	cout << "Введите нужный вам год" << endl;
+	cin >> n;
+
+	m = n / 100;
+	if (n % 100 == 0)
+	{
+		cout << n << " год принадлежит " << m << "столетию" << endl;
+	}
+	else
+	{
+		cout << n << " год принадлежит " << m + 1 << "столетию" << endl;
+	}
+
+	system("pause");
+	return 0;
+}
 
