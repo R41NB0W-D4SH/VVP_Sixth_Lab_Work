8f28cc95ee43e78e3978369ef0c86b27b4072f60
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-10-01 14:49:32 +0300

    Update task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int a, b, c;
+
+	cout << "Введите целые положительные числа A, B и C соответственно" << endl;
+	cin >> a >> b >> c;
+
+	if (c <= a && c <= b)
+	{
+		a = a * b;
+		c = c * c;
+		b = a / c;
+		a = a % c;
+		cout << "Количество целых квадаротов в прямоугольнике равно " << b << endl;
+		cout << "Площадь незанятой части прямоугольника составляет " << a << endl;
+	}
+	else
+	{
+		cout << "Квадрат не вмещается в прямоугольник" << endl;
+	}
+
+	system("pause");
+	return 0;
+}
