297cce6813033c1f7c7be700a98406d8168776b0
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-10-01 13:37:41 +0300

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	string week[7] = { "Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота" };
+	int k;
+
+	cout << "Введите какой-нибудь день в году числом (1 - 365)" << endl;
+	cin >> k;
+	k = k % 6;
+
+	if (k != 3)
+	{
+		cout << "День недели " << week[k] << endl;
+	}
+	else
+	{
+		cout << "Это " << week[k] << " чел" << endl;
+	}
+
+	system("pause");
+	return 0;
+}
